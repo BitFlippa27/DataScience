cb5866bf56116976711664866d9ff90a37b785ce
Dennis <dennis@elliot27.local>
2019-04-11 23:39:30 +0200

    First Upload

---
+++ JuypterNotebooks/First.py
@@ -0,0 +1,33 @@
+
+# coding: utf-8
+
+# # Secret Universe
+
+# My Title
+# 
+
+# In[1]:
+
+
+1 + 1
+
+
+# In[2]:
+
+
+x = "Hello universe"
+
+
+# In[3]:
+
+
+print(x)
+
+
+# <font color="red"> RED TEXT COLOR</font>
+
+# **bold**
+
+# _italics_
+
+# <a href=https://www.ibm.com/watson><img src=https://www.ibm.com/watson/assets/duo/img/common/avatar_purple.png>
